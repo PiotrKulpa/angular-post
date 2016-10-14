a9462a8dfdcb336b2884cd8c02b767747697b26b
Piotr Kulpa <p.kulpa@onet.eu>
2016-10-14 09:32:16 +0200

    01 Commit - basic app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPostApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularPostApp
+ */
+angular.module('angularPostApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularPostApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularPostApp
+ */
+angular.module('angularPostApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
